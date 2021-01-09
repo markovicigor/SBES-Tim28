704136c2054af440bb8a610c1a737e4a95605f67
vcamagic <vcamagic@yahoo.com>
2021-01-09 14:00:23 +0100

    BackupServer

---
+++ SyslogServer/BackupService/BackupServer.cs
@@ -0,0 +1,64 @@
+﻿using Common;
+using SecurityManager;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupService
+{
+    public class BackupServer : IBackupServer
+    {
+        
+        public string BackupLog(Dictionary<int, Event> dictionary)
+        {
+            string path = @"..\..\..\BackupService\backupServer.txt";
+            TextWriter tw = new StreamWriter(path, false);
+
+            string podaci = "";
+            foreach(Event e in dictionary.Values)
+            {
+            podaci += e.id + ";" + e.criticality + ";" + e.timestamp + ";" + e.source + ";" + e.message + ";" + e.eState + "\n";
+             
+            tw.WriteLine(e.id + ";" + e.criticality + ";" + e.timestamp + ";" + e.source + ";" + e.message + ";" + e.eState);
+
+            }
+
+            tw.Close();
+
+            return podaci;
+        }
+
+        public void SendMessage(string message, byte[] sign)
+        {
+            
+            //kad je u pitanju autentifikacija putem Sertifikata
+            string clienName = Formatter.ParseName(ServiceSecurityContext.Current.PrimaryIdentity.Name);
+
+            string clientNameSign = "servis_sign";
+           
+            X509Certificate2 certificate = CertManager.GetCertificateFromStorage(StoreName.TrustedPeople,
+                StoreLocation.LocalMachine, clientNameSign);
+
+            /// Verify signature using SHA1 hash algorithm
+            if (DigitalSignature.Verify(message, HashAlgorithm.SHA1, sign, certificate))
+            {
+                Console.WriteLine("Sign is valid");
+            }
+            else
+            {
+                Console.WriteLine("Sign is invalid");
+            }
+        }
+
+        public void TestCommunication()
+        {
+            Console.WriteLine("Komunikacija je uspostavljena sa Backup serverom ");
+        }
+    }
+}
---
+++ SyslogServer/BackupService/Program.cs
@@ -0,0 +1,77 @@
+﻿using Common;
+using SecurityManager;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string srvCertCN = Formatter.ParseName(WindowsIdentity.GetCurrent().Name);
+            NetTcpBinding binding = new NetTcpBinding();          
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Certificate;
+            string address = "net.tcp://localhost:9003/BackupServer";
+            ServiceHost host = new ServiceHost(typeof(BackupServer));
+            host.AddServiceEndpoint(typeof(IBackupServer), binding, address);
+
+            ///Custom validation mode enables creation of a custom validator - CustomCertificateValidator
+            host.Credentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.Custom;
+            host.Credentials.ClientCertificate.Authentication.CustomCertificateValidator = new ServiceCertValidator();
+
+            ///If CA doesn't have a CRL associated, WCF blocks every client because it cannot be validated
+            host.Credentials.ClientCertificate.Authentication.RevocationMode = X509RevocationMode.NoCheck;
+
+            ///Set appropriate service's certificate on the host. Use CertManager class to obtain the certificate based on the "srvCertCN"
+            host.Credentials.ServiceCertificate.Certificate = CertManager.GetCertificateFromStorage(StoreName.My, StoreLocation.LocalMachine, srvCertCN);
+
+            host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+
+
+            ServiceDebugBehavior debug = host.Description.Behaviors.Find<ServiceDebugBehavior>();
+            if (debug == null)
+            {
+                host.Description.Behaviors.Add(
+                     new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+            }
+            else
+            {
+
+                if (!debug.IncludeExceptionDetailInFaults)
+                {
+                    debug.IncludeExceptionDetailInFaults = true;
+                }
+            }
+
+            try
+            {
+               
+                host.Open();
+                Console.WriteLine("Backup server je otvoren.. "); 
+                Console.ReadLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("[StackTrace] {0}", e.StackTrace);
+            }
+            finally
+            {
+                 host.Close();
+
+            }
+           
+        }
+    }
+}
